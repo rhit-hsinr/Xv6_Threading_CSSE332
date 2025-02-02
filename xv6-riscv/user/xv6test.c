1700fdb6c1ed894a30cddf6cc0f7fc3047ae8e36
Richard Hsin <hsinr@rose-hulman.edu>
2025-02-02 16:46:24 -0500

    copying xv6 to repo

---
+++ xv6-riscv/user/xv6test.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  printf("Hello from my test case in xv6\n");
+  uint64 p = 0xdeadbeef;
+  spoon((void*)p);
+  exit(0);
+}
