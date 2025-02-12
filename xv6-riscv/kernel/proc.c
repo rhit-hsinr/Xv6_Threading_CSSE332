02e9b852f5ac7c75a991541b647a812b75425b59
Reilly <mooneyrr@RHIT-R912GADS.localdomain>
2025-02-11 20:48:26 -0500

    added fork to clone

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -330,7 +330,7 @@ fork(void)
 void
 reparent(struct proc *p)
 {
-  struct proc *pp;
+  struct proc *pp;clon
 
   for(pp = proc; pp < &proc[NPROC]; pp++){
     if(pp->parent == p){
@@ -691,9 +691,54 @@ uint64 spoon(void *arg)
 
 int clone(void (*func)(void*), void *arg, void *stack)
 {
-  printf("clone has been called: %p, argument: %p, stack: %p\n", func, arg, stack);
-  printf("This call has not been implemented yet!\n");
-  return 666;
+  int i, pid;
+  struct proc *np;
+  struct proc *p = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  // Copy user memory from parent to child.
+  if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
+    freeproc(np);
+    release(&np->lock);
+    return -1;
+  }
+  np->sz = p->sz;
+
+  // copy saved user registers.
+  *(np->trapframe) = *(p->trapframe);
+
+  // Cause fork to return 0 in the child.
+  np->trapframe->a0 = 0;
+
+  // increment reference counts on open file descriptors.
+  for(i = 0; i < NOFILE; i++)
+    if(p->ofile[i])
+      np->ofile[i] = filedup(p->ofile[i]);
+  np->cwd = idup(p->cwd);
+
+  safestrcpy(np->name, p->name, sizeof(p->name));
+
+  pid = np->pid;
+
+  release(&np->lock);
+
+  acquire(&wait_lock);
+  np->parent = p;
+  release(&wait_lock);
+
+  acquire(&np->lock);
+  np->state = RUNNABLE;
+  release(&np->lock);
+
+  return pid;
+  
+  //printf("clone has been called: %p, argument: %p, stack: %p\n", func, arg, stack);
+  //printf("This call has not been implemented yet!\n");
+  //return 666;
 }
 
 int join(int pid, void **stack)
